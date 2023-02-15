9b7f67639fc971e3e4f2d92f55c46286bf161cd9
matcoco <b_jayson@hotmail.com>
2023-02-15 19:20:12 +0100

    fix doublon + amelioration

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
 let tab = []
 let resultat = "";
 let list = ""
+let countGbook = 0
 
-function getList(){
+function getList() {
     list = document.querySelector("#excel-list").value
-    if(list) convertToRequest();
+    if (list) convertToRequest();
 }
 
-function convertToRequest(){
+function convertToRequest() {
     resultat = '#IN ('
     tab = list.trim().split("\n")
-    tab.pop();
-    for(let i = 0; i < tab.length; i++){
-        if(tab[i]) resultat += `'${tab[i]}',`
-        if(i === tab.length - 1) resultat += `'${tab[i]}')`
-    }
 
+    for (let i = 0; i < tab.length; i++) {
+        if (tab[i]) {
+            resultat += `'${tab[i]}'`
+            countGbook++
+        }
+        if (i < tab.length - 1 && tab[i]) resultat += `,`
+        if (i === tab.length - 1) resultat += `)`
+    }
     displayResult(resultat);
 }
 
-function removeList(){
+function removeList() {
     document.querySelector("#excel-list").value = ""
     document.querySelector("#excel-list-result").value = ""
+    document.querySelector("#countGbook").innerHTML = ""
+    countGbook = 0
 }
 
-function displayResult(str){
+function displayResult(str) {
     document.querySelector("#excel-list-result").value = str
+    document.querySelector("#countGbook").innerHTML = countGbook
 }
 
-
+document.getElementById('btn-copy').addEventListener('click', function () {
+    var text = document.getElementById('excel-list-result');
+    text.select();
+    document.execCommand('copy');
+})
